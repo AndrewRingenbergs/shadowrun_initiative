39ab03456c4f038bc7818ad0a5a146a301f5ad77
Andrew Ringenbergs <a.ringenbergs@gmail.com>
2016-12-18 11:36:38 +1100

    Base commit of change to angular framework based on commit by  James Duffield.

--- app.js
+++ app.js
@@ -1,39 +1,88 @@
-function guid() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
-}
+var app = angular.module("myApp", []); 
 
-function char(name,initBase,initCur, body, agility, reaction, strength, willpower, logic, intuition, charisma, edge, essence, magres) {
-	return {
-		guid: guid(),
-		name: name,
-		initBase: initBase,
-		initCur: initCur,
-		healthPMax: 6,
-		damageP: 0,
-		activity: "active",
-		body: body,
-		agility: agility,
-		reaction: reaction,
-		strength: strength,
-		willpower: willpower,
-		logic: logic,
-		intuition: intuition,
-		charisma: charisma,
-		edge: edge,
-		essence: essence,
-		magres: magres,
-		initiative: reaction+intuition
+app.controller("myCtrl", function($scope) {  
+
+	function s4() {
+		return Math.floor((1 + Math.random()) * 0x10000)
+		.toString(16)
+		.substring(1);
+		};
+
+	function guid() {
+		return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+		};
+
+	function createChar(name, initBase, initCur) {
+		return {
+			guid: guid(),
+			name: name,
+			initBase: initBase,
+			initCur: initCur,
+			healthPMax: 6,
+			damageP: 0,
+			activity: "active"
+		};
 	}
-}
 
-charArr = [new char("Alpha",18,3,12,5,6,2,1,1,1,1,1,6,0), new char("Beta",5,4,5,2,3,1,1,1,1,1,1,6,0)];
+	$scope.addChar = function() {
+		console.log('Creating New Char');
+		newChar = createChar("NA", 0, 0);
+		$scope.charArr.push(newChar);
+	}
 
+	$scope.healthBoxClick = function(char, h) {
+		// Set damageP
+		char.damageP = h;
+	}
+
+	$scope.nextInit = function() {
+		console.log('Next Initiative being calculated');
+
+		// Set current active char to acted
+		var activeCharArrPrior = $scope.charArr.filter(function (char) { return char.activity === "selected"; })[0];
+		if (activeCharArrPrior) {
+			activeCharArrPrior.activity = 'acted';
+		}
+
+		// Get list of non-acted characters & find the one with highest current initiative
+		var activeCharArr = $scope.charArr.filter(function (char) { return (char.activity === 'active' && char.initCur > 0); });
+
+		if (activeCharArr.length > 0) {
+			var targetChar = activeCharArr.reduce(function(a, b){ return a.initCur > b.initCur ? a : b });
+			// Set targetChar.active to true
+			targetChar.activity = 'selected';
+		}
+		else {
+			// Start new pass
+			$scope.charArr.forEach( function (obj) { obj.initCur = obj.initCur - 10; });
+			$scope.charArr.forEach( function (obj) { obj.activity = 'active' });
+			
+			// New round if all characters have negative/0 initiative
+			if ($scope.charArr.filter(function (objArr) { return objArr.initCur > 0; }).length == 0) {
+				$scope.charArr.forEach( function (obj) { obj.initCur = obj.initBase; });
+			}
+			$scope.nextInit();
+		}
+
+		console.log($scope.charArr)
+	}
+
+	$scope.deleteChip = function(char) {
+		console.log("Deleting chip", char);
+		console.log($scope.charArr);
+		$scope.charArr.splice($scope.charArr.indexOf(char),1);
+	}
+
+	$scope.charArr = [];
+	$scope.charArr.push(createChar("Alpha", 18, 3));
+	$scope.charArr.push(createChar("Beta", 5, 4));
+
+	console.log("Page load finished");
+
+});
+
+
+/*
 function load() {
 
 	createCharTable();
@@ -254,3 +303,4 @@ function dataInputOutputUpdate(charObj) { // needs testing
 		dataInputs[i].textContent = charObj[dataInputs[i].dataset.contents];
 	}
 }
+*/
