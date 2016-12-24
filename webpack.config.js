2c694e5895fee65828dc408eb865acf1d48d5874
amedw <mr.amedw@gmail.com>
2016-12-24 14:05:14 +1100

    Moved packages around and added css stuff

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,30 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var ENV = process.env.npm_lifecycle_event;
 var isTest = ENV === 'test' || ENV === 'test-watch';
 var isProd = ENV === 'build';
 
+function isExternal(module) {
+  var userRequest = module.userRequest;
+
+  if (typeof userRequest !== 'string') {
+    return false;
+  }
+
+  return userRequest.indexOf('bower_components') >= 0 ||
+    userRequest.indexOf('node_modules') >= 0 ||
+    userRequest.indexOf('libraries') >= 0;
+}
+
 module.exports = function() {
   var config = {};
 
   config.entry = isTest ? {} : {
-    app: './app.js'
+    app: './src/app.js',
+    styles: './src/styles/style.css'
   };
 
   config.output = isTest ? {} : {
@@ -21,23 +35,41 @@ module.exports = function() {
     chunkFilename: isProd ? '[name].[hash].js' : '[name].bundle.js'
   };
 
-  config.loaders = [{
-      test: /\app.js$/,
-      loader: 'webpack-append',
-      query: 'require("angular")'
-    }
-  ]
+  config.module = {}
+  config.module.loaders = [{
+    test: /\.css$/,
+    loader: ExtractTextPlugin.extract("style-loader", "css-loader"  )
+  }];
 
-  config.plugins = config.plugins || []
+  config.plugins = [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendors',
+      minChunks: function(module) {
+        return isExternal(module);
+      }
+    })
+  ];
+
   if (!isTest) {
     config.plugins.push(
         new HtmlWebpackPlugin({
-          template: './index.html',
+          template: './src/index.html',
           inject: 'body'
         }),
         new ExtractTextPlugin('[name].[hash].css', {disable: !isProd}))
   }
 
+  if(isProd) {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        new CopyWebpackPlugin([{ from: __dirname + '/src/public' }])
+        );
+  }
+
+  config.devServer = {
+    contentBase: './src/public',
+    stats: 'minimal'
+  };
 
   return config;
-  }();
+}();
